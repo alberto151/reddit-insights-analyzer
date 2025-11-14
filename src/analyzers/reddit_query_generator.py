9b92162b558e20ea284397c2e11607990724517e
land-p <ionlandecoure@gmail.com>
2025-11-14 09:26:55 -0500

    feat: add src/analyzers/reddit_query_generator.py

---
+++ src/analyzers/reddit_query_generator.py
@@ -0,0 +1,4 @@
+thondef generate_query(query):
+    # This is a simple function to generate a Reddit search query.
+    # In a production environment, this can be more sophisticated.
+return f"site:reddit.com {query}"
